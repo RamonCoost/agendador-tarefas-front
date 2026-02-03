1d2e3d22801c5de041326494c980f7d16a86c985
RamonCoost <ramoncoost@gmail.com>
2026-02-03 16:38:27 -0300

    criado os metodos de salvar e editar o telefone e o endereco

---
+++ src/app/shared/components/modal-dialog/modal-dialog.component.ts
@@ -0,0 +1,70 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+export interface DialogField {
+  name: string;
+  label: string;
+  value?: string | number;
+  type?: string;
+  validators?: any[]
+}
+
+interface DialogData {
+  title: string
+  formConfig: DialogField[]
+}
+
+@Component({
+  selector: 'app-modal-dialog',
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogTitle,
+    ReactiveFormsModule
+  ],
+  templateUrl: './modal-dialog.component.html',
+  styleUrl: './modal-dialog.component.scss'
+})
+export class ModalDialogComponent {
+  readonly formBuilder = inject(FormBuilder)
+  readonly dialogRef = inject(MatDialogRef<ModalDialogComponent>);
+  readonly data = inject<DialogData>(MAT_DIALOG_DATA);
+
+  fields: DialogField[] = this.data.formConfig;
+
+  private buildConstrols(): Record<string, any> {
+    const controls: Record<string, any> = {}
+
+    this.fields.forEach(field => {
+      controls[field.name] = [field.value ?? '', field.validators || []]
+    });
+
+    return controls;
+  }
+
+  form: FormGroup = this.formBuilder.group(this.buildConstrols());
+
+  onSave() {
+    this.dialogRef.close(this.form.value)
+   }
+
+  OnCancel(): void {
+    this.dialogRef.close();
+  }
+}
